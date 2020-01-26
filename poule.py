c751189065664efd5cbd20eb533de8c142fd6124
Scaule <scaule@clever-age.com>
2020-01-26 14:09:34 +0100

    work on log and prevent open or close 2 times

--- poule.py
+++ poule.py
@@ -1,29 +1,51 @@
 from flask import Flask
 import RPi.GPIO as GPIO
 import time
+import datetime
+
+#File to write status log 
+FILE_NAME = "poule.txt"
+
+#TIME TO OPEN OR CLOSE DOOR 
+SLEEP_TIME = 40
 
 app = Flask(__name__)
-@app.route('/stop')
-def stop():
-    GPIO.output(12, GPIO.LOW)
-    GPIO.output(11, GPIO.LOW)
-    return 'Stop'
+def writeStatus (status) :
+    statusFile = open (FILE_NAME, 'a' )
+    statusFile.write(status +' | '+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+
+def readLastStatus () :
+    fileHandle = open (FILE_NAME, 'r' )
+    lineList = fileHandle.readlines()
+    fileHandle.close()
+    return lineList[-1].split("|")[0].strip()
+
 @app.route('/open')
-def open():
+def openDoor():
+    if readLastStatus() == 'open':
+        return 'Already open'
+    writeStatus('open')    
     GPIO.output(12, GPIO.LOW)
     GPIO.output(11, GPIO.HIGH)
-    time.sleep(40)
+    time.sleep(SLEEP_TIME)
     GPIO.output(11, GPIO.LOW)
     return 'Open'
+
 @app.route('/close')
-def close():
+def closeDoor():
+    print readLastStatus()
+    if readLastStatus() == 'close':
+        return 'Already close'
+    writeStatus('close')
     GPIO.output(11, GPIO.LOW)
     GPIO.output(12, GPIO.HIGH)
-    time.sleep(40)
+    time.sleep(SLEEP_TIME)
     GPIO.output(12, GPIO.LOW)
-    return 'close'
+    return 'Close'
+
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=8033, host='0.0.0.0')
+
